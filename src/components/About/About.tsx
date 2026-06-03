7d97c1571d990254c29a55fae2ba5aa55a0ee16e
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-06-03 23:33:13 +0300

    style(about): apply orange brand color to About heading icon

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ import "./About.css";
 export function About() {
     return (
         <div id="about" className="nav-section">
-            <SectionHeading title="About Me" icon={<FaUser />} />
+            <SectionHeading title="About Me" icon={<FaUser />} iconColor="var(--heading-about)" />
             <section className="panel">
                 <h2 className="align-left">Who am I?</h2>
                 <div className="about-text stagger-container">
